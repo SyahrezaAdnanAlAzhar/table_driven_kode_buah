bc64db985e0c75af6b902ecd893b3da831be7c08
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2024-03-13 07:22:34 +0700

    penambahan class KodeBuah

--- program_kode_buah/Program.cs
+++ program_kode_buah/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worlda!");
+﻿using System;
+
+public class KodeBuah
+{
+    public enum Buah { Apel , Aprikot , Alpukat , Pisang ,
+        Paprika , Blackberry , Ceri , Kelapa , Jagung ,
+        Kurma , Durian , Anggur , Melon , Semangka };
+
+    public static String getKodeBuah(Buah buah)
+    {
+        String[] kodeBuah = { "A00" , "B00", "C00", "D00", "E00", "F00", "H00",
+            "I00", "J00", "K00", "L00", "M00", "N00", "O00", };
+
+        return kodeBuah[(int) buah];
+    }
+}
